f60129d218a4ab260a08e4dea3e82c53670dbb12
Issam <iba10@pitt.edu>
2022-09-10 14:12:06 -0400

    Added random number generation!

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,14 +1,15 @@
 import os
+import random
 
-lowerBound = 1
-upperBound = 10
-answerNum  = 5
+lowerBound = random.randint(0,50)
+upperBound = lowerBound + 10
+answerNum  = random.randint(lowerBound,upperBound)
 
 #Clear Console
 os.system('clear')
 
-print("Guess the number: A fun little game!")
-print("It's between " + str(lowerBound) + " and " + str(upperBound) + "! What do you think it is? ")
+print("Guess the number: A fun little game!\n")
+print("It's between " + str(lowerBound) + " and " + str(upperBound) + "! What do you think it is?\n")
 theGuess = input("Your guess: ")
 
 if (theGuess == str(answerNum)):
