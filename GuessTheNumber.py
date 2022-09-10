17021ae3e16d292118a2348404746fa066d0f0c7
Issam <iba10@pitt.edu>
2022-09-10 14:33:49 -0400

    Added a loop and some guides!

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,18 +1,51 @@
 import os
 import random
 
-lowerBound = random.randint(0,50)
-upperBound = lowerBound + 10
-answerNum  = random.randint(lowerBound,upperBound)
-
-#Clear Console
+# Clear Console
 os.system('clear')
 
-print("Guess the number: A fun little game!\n")
-print("It's between " + str(lowerBound) + " and " + str(upperBound) + "! What do you think it is?\n")
-theGuess = input("Your guess: ")
+# Variable Declaration
+lowerBound = random.randint(0,50)
+upperBound = lowerBound + random.randint(10,50)
+answerNum  = random.randint(lowerBound,upperBound)
+guesses    = 0
+maxguesses = 5
 
-if (theGuess == str(answerNum)):
-    print("You win!")
-else:
-    print("You lose!")
+print("Guess the number: A fun little game!\n")
+print("It's between " + 
+    str(lowerBound) + " and " + 
+    str(upperBound) + "!\n")
+
+while (guesses < maxguesses):
+    # Let them know how many guesses they have left
+    print("Guesses left: " + str(3 - guesses) + "\n")
+
+    # Ask for an answer
+    theGuess = int(input("Your guess: "))
+    print()
+    
+    # Logic for game
+
+    # If we are out of bounds, tell them and return their guess count!
+    if (theGuess < lowerBound or theGuess > upperBound ):
+        print("That's out of bounds! It's between " + 
+            str(lowerBound) + " and " + 
+            str(upperBound) + "!\n")
+        continue
+    
+    # If we are the right answer let them know and leave the loop!
+    elif (theGuess == answerNum):
+        print("You win!\n")
+        break
+
+    # If we are too low, tell them to aim higher!
+    elif (theGuess < answerNum):
+        print("Too low! Aim higher!\n")
+
+    # If we are too high, tell them to aim lower!
+    elif (theGuess > answerNum):
+        print("Too high! Aim lower!\n")
+
+    # Add a guess
+    guesses += 1
+
