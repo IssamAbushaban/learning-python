e36c147023330907bd2d210117c6d2d03c89a223
Issam <iba10@pitt.edu>
2022-09-10 14:02:26 -0400

    Added a clear (flushing screen)

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,13 +1,17 @@
-if __name__ == '__main__':
-    lowerBound = 1
-    upperBound = 10
-    answerNum  = 5
+import os
 
-    print("Guess the number: A fun little game!")
-    print("It's between " + str(lowerBound) + " and " + str(upperBound) + "! What do you think it is? ")
-    theGuess = input("Your guess: ")
+lowerBound = 1
+upperBound = 10
+answerNum  = 5
 
-    if (theGuess == str(answerNum)):
-        print("You win!")
-    else:
-        print("You lose!")
+#Clear Console
+os.system('clear')
+
+print("Guess the number: A fun little game!")
+print("It's between " + str(lowerBound) + " and " + str(upperBound) + "! What do you think it is? ")
+theGuess = input("Your guess: ")
+
+if (theGuess == str(answerNum)):
+    print("You win!")
+else:
+    print("You lose!")
