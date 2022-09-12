5bf653952256709ba238fac3873ba0427d7c49f4
Issam <iba10@pitt.edu>
2022-09-12 13:47:29 -0400

    It now prints using Beautiful Soup!!!

--- WebScraping.py
+++ WebScraping.py
@@ -1,17 +1,27 @@
 import os
+# This is to help us make HTTP request
 import requests
+# This is to make the text coming from the site more readible
+from bs4 import BeautifulSoup 
 
 # Clear Console
 os.system('clear')
 
-reqURL = "https://www.geeksforgeeks.org/python-programming-language/"
+# The Site I will scrape from
+reqURI = "https://www.geeksforgeeks.org/python-programming-language/"
 
 # lets make a GET request
-print("Requesting Scrape Of " + reqURL + "\n")
-reqGET = requests.get(reqURL)
+print("Requesting Scrape Of " + reqURI + "\n")
+reqGET = requests.get(reqURI)
 
 # Did it succeed?
 print("Status Returned: " + str(reqGET.status_code) + "\n")
-if (reqGET.status_code == 200):
-    # print content of request
-    print(str(reqGET.content) + "\n")
+if (reqGET.status_code != 200):
+    print("Looks like we are done here :(\n")
+    raise SystemExit
+    
+# Use Beautiful Soup to clean that html!
+soupOfURI = BeautifulSoup(reqGET.content, 'html.parser')
+
+# Print content of request
+print(soupOfURI.prettify() + "\n")
